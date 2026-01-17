22a41ad6d41eff416ba7fa52944979d1f3f26782
meShahabaj <shahabaj773@gmail.com>
2026-01-18 00:05:32 +0530

    chore: comment out proxy middleware code for future reference

--- frontend/proxy.ts
+++ frontend/proxy.ts
@@ -1,44 +1,44 @@
-// middleware.ts
-import { NextResponse } from "next/server";
-import type { NextRequest } from "next/server";
+// // middleware.ts
+// import { NextResponse } from "next/server";
+// import type { NextRequest } from "next/server";
 
-const BACKEND_API = process.env.NEXT_PUBLIC_BACKEND_API
-const FRONTEND_API = process.env.NEXT_PUBLIC_FRONTEND_API
+// const BACKEND_API = process.env.NEXT_PUBLIC_BACKEND_API
+// const FRONTEND_API = process.env.NEXT_PUBLIC_FRONTEND_API
 
-const ALLOWED_ORIGINS = [
-    BACKEND_API, FRONTEND_API
-];
+// const ALLOWED_ORIGINS = [
+//     BACKEND_API, FRONTEND_API
+// ];
 
 
-// Middleware function
-export function proxy(req: NextRequest) {
-    const origin = req.headers.get("origin");
+// // Middleware function
+// export function proxy(req: NextRequest) {
+//     const origin = req.headers.get("origin");
 
-    // If it's a CORS request
-    if (origin) {
-        if (!ALLOWED_ORIGINS.includes(origin)) {
-            return NextResponse.json(
-                { error: "Origin not allowed" },
-                { status: 403 }
-            );
-        }
-    }
+//     // If it's a CORS request
+//     if (origin) {
+//         if (!ALLOWED_ORIGINS.includes(origin)) {
+//             return NextResponse.json(
+//                 { error: "Origin not allowed" },
+//                 { status: 403 }
+//             );
+//         }
+//     }
 
-    // Get token from cookies (HttpOnly)
-    const token = req.cookies.get("token")?.value;
+//     // Get token from cookies (HttpOnly)
+//     const token = req.cookies.get("token")?.value;
 
-    // If no token, redirect to login page
-    if (!token) {
-        return NextResponse.redirect(new URL("/login", req.url));
-    }
+//     // If no token, redirect to login page
+//     if (!token) {
+//         return NextResponse.redirect(new URL("/login", req.url));
+//     }
 
-    // If token exists, continue to the requested page
-    return NextResponse.next();
-}
+//     // If token exists, continue to the requested page
+//     return NextResponse.next();
+// }
 
-// Apply middleware only to protected routes
-export const config = {
-    matcher: [
-        "/((?!login|signup|_next|api).*)", // negative lookahead
-    ]
-};
+// // Apply middleware only to protected routes
+// export const config = {
+//     matcher: [
+//         "/((?!login|signup|_next|api).*)", // negative lookahead
+//     ]
+// };
